b84cd30d1ff287b85d677d6adc915dcf62525081
Parthasarathi-Das <abc312967@gmail.com>
2025-04-07 18:58:06 +0530

    Deep Learning module is added

--- app.py
+++ app.py
@@ -1,12 +1,34 @@
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+import hope
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+mymail ="abc312967@gmail.com"
+pw ="partha1223"
 
+class Visit(db.Model):
+    sno = db.Column(db.Integer, primary_key = True)
+    visitor_email  = db.Column(db.String(50), nullable = False)
+    visitor_name = db.Column(db.String(200), nullable = False)
+    stock = db.Column(db.String(20), nullable = False)
+    days = db.Column(db.Integer, nullable = False)
+    time = db.Column(db.DateTime, default = datetime.now())
+    def __repr__(self):
+        return f"{self.sno} - {self.visitor_email} - {self.time}"
+
+class Feedback(db.Model):
+    sno = db.Column(db.Integer, primary_key = True)
+    user_name = db.Column(db.String(200), nullable = False)
+    user_email  = db.Column(db.String(50), nullable = False)
+    desc  = db.Column(db.String(500), nullable = False)
+    rate = db.Column(db.Integer, nullable = False)
+    time = db.Column(db.DateTime, default = datetime.now())
+    def __repr__(self):
+        return f"{self.sno} - {self.user_email} - {self.time}"
 
 @app.route('/')
 def index():
---
+++ hope.py
@@ -0,0 +1,89 @@
+import yfinance as yf
+import numpy as np
+import pandas as pd
+from bokeh.models import DatetimeTickFormatter
+from bokeh.plotting import figure, output_file, save
+from keras import Sequential
+from keras.api.layers import LSTM, Dense
+from sklearn.preprocessing import MinMaxScaler
+import datetime 
+
+def ticker_validate(ticker):
+    try:
+        data = yf.Ticker(ticker)
+        info = data.info
+        return 'shortName' in info
+    except Exception:
+        return False
+
+def prepare_data(scaled_data, base_days):
+    x_data = []
+    y_data = []
+
+    for i in range(base_days, len(scaled_data)):
+        x_data.append(scaled_data[i-base_days: i])
+        y_data.append(scaled_data[i])
+        train_factor = 0.7 #Optimum
+    x_train, x_test = x_data[:int(train_factor*len(x_data))], x_data[int(train_factor*len(x_data)):]
+    y_train, y_test = y_data[:int(train_factor*len(y_data))], y_data[int(train_factor*len(y_data)):]
+    x_train = np.array(x_train)
+    y_train = np.array(y_train)
+    x_test = np.array(x_test)
+    y_test = np.array(y_test)
+    return x_train, x_test, y_train, y_test
+
+def predict_stock(ticker_name, days):
+    end = datetime.datetime.now()
+    start = datetime.datetime.now() - datetime.timedelta(days=5*365)
+    stockdf = yf.download(ticker_name, start, end)
+    stockdf = stockdf[["Close"]]
+    #Scaling the data
+    scaler = MinMaxScaler()
+    scaled_stockdf = scaler.fit_transform(stockdf)
+    base_days = 100
+    x_train, x_test, y_train, y_test = prepare_data(scaled_stockdf, base_days)
+
+    model = Sequential([
+        LSTM(128, return_sequences=True, input_shape=(base_days, 1)),
+        LSTM(64, return_sequences=False),
+        Dense(25),
+        Dense(1)
+    ])
+    model.compile(optimizer="adam", loss="mean_squared_error")
+    model.fit(x_train, y_train, batch_size= 5, epochs=10, validation_data=(x_test,y_test))
+
+    last_100 = scaled_stockdf[-100:].reshape(1,-1,1)
+    future_predictions = []
+
+    for i in range(days):
+        next_day_pred = model.predict(last_100)
+        future_predictions.append(scaler.inverse_transform(next_day_pred))
+        last_100 = np.append(last_100[:,1:,:], next_day_pred.reshape(1,1,-1), axis = 1)
+
+    start_date = datetime.date.today()
+    dates = [(start_date + datetime.timedelta(days=i)).strftime("%d-%m-%Y") for i in range(30)]
+
+    p = figure(title=f"{ticker_name} Stock Prediction for {days} days",
+            height=500, width=1000,
+            x_axis_label="Date", y_axis_label="Close",
+            toolbar_location=None, sizing_mode="scale_both",
+            x_axis_type="datetime")
+    
+    datetime_dates = [datetime.datetime.strptime(date, "%d-%m-%Y") for date in dates]
+    p.line(datetime_dates, future_predictions, line_width=2)
+    p.xaxis[0].formatter = DatetimeTickFormatter(
+        days="%d-%m-%Y",
+        months="%d-%m-%Y",
+        years="%d-%m-%Y"
+    )
+    output_file("templates\\plot.html")
+    save(p)
+    start_date = datetime.date.today()
+    dates = [(start_date + datetime.timedelta(days=i)).strftime("%d %B, %Y") for i in range(30)]
+    values = [array[0][0] for array in future_predictions]
+
+    df = pd.DataFrame({
+        'Date': dates,
+        'Prediction': values
+    })
+    return df
